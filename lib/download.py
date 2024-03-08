8bb168a15ceb7bc5bf04cb9967258df89a56251c
Andrew Laptev <wtfthefirefox@gmail.com>
2024-03-09 01:28:16 +0300

    added export file from selected folder

--- lib/download.py
+++ lib/download.py
@@ -1,31 +1,35 @@
 import os
 
-from flask import send_file
+from flask import send_file, request
 from flask_restx import Resource
-from marshmallow.exceptions import ValidationError
+from .exceptions import NotFoundError
 
 from .api import api
-from .models import DownloadFile
 
 from .config import config
 
 TEST_CONFIG_PATH = "test_files"
-cur_route = api.namespace("downloading", description="Profile API", path="/download/")
+cur_route = api.namespace("downloading", description="download API", path="/download/")
 
 @cur_route.route("/file/<string:file_name>/")
-class ProfileView(Resource):
+class DownladFile(Resource):
     """
-    Download file with name
+    Download file with name in selected folder
     """
 
     def get(self, file_name):
+        folder_path = request.args.get("folder_path")
+        file_path = os.path.join(folder_path if folder_path else "", file_name)
         file_name_base_path = ""
+        # TODO: Add check for .. in folder_path
         if config["is_test"]:
-           file_name_base_path = os.path.join(os.path.join(os.getcwd(), TEST_CONFIG_PATH), file_name) 
+           file_name_base_path = os.path.join(os.path.join(os.getcwd(), TEST_CONFIG_PATH), file_path) 
         else:
-            file_name_base_path = os.path.join(config["export_dir"], file_name)
-
-        if not os.path.exists(file_name_base_path):
-            raise ValidationError(f"File with name {file_name} does not exists")
-
+            file_name_base_path = os.path.join(config["export_dir"], file_path)
+        if not os.path.exists(file_name_base_path) or not os.path.isfile(file_name_base_path):
+            if folder_path:
+                raise NotFoundError(f"File with name {file_name} in folder {folder_path} does not exists")
+            else:
+                raise NotFoundError(f"File with name {file_name} does not exists")
+    
         return send_file(file_name_base_path, as_attachment=True)
--- lib/ut/test_download.py
+++ lib/ut/test_download.py
@@ -2,3 +2,12 @@ def test_download_file(client):
     response = client.get("api/v1/download/file/2024-03-03T21-52-01.mp4")
     assert response.status_code == 308
     assert str(response.data, "utf-8") == """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n<title>Redirecting...</title>\n<h1>Redirecting...</h1>\n<p>You should be redirected automatically to target URL: <a href="http://localhost/api/v1/download/file/2024-03-03T21-52-01.mp4/">http://localhost/api/v1/download/file/2024-03-03T21-52-01.mp4/</a>.  If not click the link."""
+
+def test_download_file_from_folder(client):
+    response = client.get("api/v1/download/file/file.txt?folder_path=another_folder")
+    assert response.status_code == 308
+    assert str(response.data, "utf-8") == """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n<title>Redirecting...</title>\n<h1>Redirecting...</h1>\n<p>You should be redirected automatically to target URL: <a href="http://localhost/api/v1/download/file/file.txt/?folder_path=another_folder">http://localhost/api/v1/download/file/file.txt/?folder_path=another_folder</a>.  If not click the link."""
+
+def test_download_undefined_file(client):
+    response = client.get("api/v1/download/file/dniauwduaw.dwa/")
+    assert response.status_code == 404
