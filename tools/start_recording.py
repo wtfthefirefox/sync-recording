87b3f45f485ed41975e68566b694bf5c112d1f7d
Stanislav <53866520+stasray@users.noreply.github.com>
2024-04-19 21:43:02 +0200

    Recording with 2 monitors except triggers

--- tools/start_recording.py
+++ tools/start_recording.py
@@ -1,9 +1,7 @@
 import requests
 import datetime
 import asyncio
-import os
-import cv2
-
+import json
 import argparse
 
 
@@ -13,70 +11,96 @@ class Room:
         self.room_id = room_id
 
 
-def trigger(ip, api, group_key, cameras):
+time = "1/min"
+diff_secs = 33
+
+def save_room_state(room_id, state):
+    try:
+        with open('room_states.json', 'r') as file:
+            room_states = json.load(file)
+    except FileNotFoundError:
+        room_states = {}
+
+    room_states[room_id] = state
+
+    try:
+        with open('room_states.json', 'w') as file:
+            json.dump(room_states, file)
+            file.close()
+    except OSError as e:
+        print(e)
+        print("Error while creating database file! Check permissions for this folder.")
+
+
+def record_request(ip, api, group_key, cameras):
     for camera in cameras:
-        trigger_url = ip + '/' + api + '/motion/' + group_key + '/%camera%?data={"plug":"Force trigger","name":"Home-Assistant","reason":"TRY_TO_RECORD","confidence":100}'
+        trigger_url = ip + '/' + api + '/monitor/' + group_key + '/' + camera + '/record/' + time
         try:
-            print("Send trigger request to ", camera)
-            response1 = requests.get(trigger_url.replace("%camera%", camera))
-            if response1.status_code == 200:
-                now1 = datetime.datetime.now()
-                print(now1.time())
-                print('Recording started with trigger for ', camera)
-                print(response1.json())
+            print("Send record request to ", camera)
+            response = requests.get(trigger_url)
+            if response.status_code == 200:
+                now = datetime.datetime.now()
+                print(now.time())
+                print('Recording started for ', camera)
+                print(response.json())
             else:
-                print('GET request failed. Error:', response1.status_code)
+                print('GET request failed. Error:', response.status_code)
         except Exception as e:
-            print(f'Error stopping recording: {e}')
+            print(f'Error starting recording: {e}')
 
-async def record(ip, api, group_key, minutes_to_record, room):
-    """
-        Record specified minutes from selected room
 
-        If the argument `minutes_to_record` = -1 then video
-        will be record while STOP command not entered.
-
-        Parameters
-        ----------
-        minutes_to_record : int
-
-        room_id : str
-    """
+async def record(ip, api, group_key, room):
     cameras_id = room.cameras_id
+    room_id = room.room_id
     print("Start recording")
+    new_cameras_id = []
+    for camera in cameras_id:
+        new_cameras_id.append(camera + "_")
+    try:
+        with open('room_states.json', 'r') as file:
+            for line in file:
+                room_states = json.loads(line)
+                if room_id in room_states and room_states[room_id] == "recording":
+                    print("Recording is already in progress for this room.")
+                    return
+            file.close()
+    except FileNotFoundError:
+        pass
+    while True:
+        record_request(ip, api, group_key, cameras_id)
+        save_room_state(room_id, "recording")
 
-    while minutes_to_record > 0:
-        trigger(ip, api, group_key, cameras_id)
-        await asyncio.sleep(64)
-        minutes_to_record -= 1
+        await asyncio.sleep(diff_secs)
 
-    if minutes_to_record == -1:
-        stop_recording = False
-        while not stop_recording:
-            for camera in cameras_id:
-                trigger(ip, api, group_key, camera)
-            await asyncio.sleep(64)
-            user_input = input("Enter 'STOP' to stop recording: ")
-            if user_input == "STOP":
-                stop_recording = True
+        with open('room_states.json', 'r') as file:
+            for line in file:
+                room_states = json.loads(line)
+                if room_id in room_states and room_states[room_id] == "stop":
+                    print("Recording stopped for this room.")
+                    return
+            file.close()
+
+        record_request(ip, api, group_key, new_cameras_id)
+        save_room_state(room_id, "recording")
+
+        await asyncio.sleep(diff_secs)
 
 
 async def main():
     parser = argparse.ArgumentParser(description="Начало записи")
-    parser.add_argument("--ip", help="IP-адрес для доступа к Shinobi", default="http://0.0.0.0:8080")
-    parser.add_argument("--api", help="API для доступа к Shinobi", default="0")
-    parser.add_argument("--group_key", help="Ваш ключ группы", default="1")
-    parser.add_argument("--time", help="Длительность записи в минутах", default=4)
-    parser.add_argument("--room", help="Комната в Json", default=Room("abc", ["305_3"]))
-
-    parser.add_argument("--folder", help="Главная папка, в которой хранятся папки с записями",
-                        default="/home/Shinobi/videos")
-    parser.add_argument("--audio_file",
-                        help="Путь к видео, аудиозапись которого необходимо наложить на остальные записи.",
-                        default="/home/Shinobi/videos/test/main.mp4")
+    parser.add_argument("--ip", help="IP-адрес для доступа к Shinobi", default="http://84.201.177.141:8080")
+    parser.add_argument("--api", help="API для доступа к Shinobi", default="fYNVjZNfYuqmdt9DecSHeigZ7yPmX5")
+    parser.add_argument("--group_key", help="Ваш ключ группы", default="iIK9Z11DJ1")
+    parser.add_argument("--room", help="Комната в Json", default='{"abc": ["304_3"]}')
 
     args = parser.parse_args()
-    await record(args.ip, args.api, args.group_key, args.time, args.room)
+    room_json = json.loads(args.room)
+    room_id = list(room_json.keys())[0]
+    cameras_id = room_json[room_id]
+
+    room = Room(room_id, cameras_id)
+
+    await record(args.ip, args.api, args.group_key, room)
 
 
 if __name__ == '__main__':
