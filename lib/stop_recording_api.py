14f7e892556bac7331f91ab1775cb902307445d1
Stanislav <53866520+stasray@users.noreply.github.com>
2024-05-16 22:16:41 +0200

    Update stop_recording_api.py

--- lib/stop_recording_api.py
+++ lib/stop_recording_api.py
@@ -5,7 +5,7 @@ import datetime
 import asyncio
 import json
 
-from test.config import config
+from .config import config
 from .api import api
 
 cur_route = api.namespace('record_stop', description='Start recording of camera / room')
